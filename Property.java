163f7f927b4e9454e9c924d86bdfe373537d2b7d
ngoquangkhiem <khiem.ngo.cit20@eiu.edu.vn>
2025-04-26 09:11:16 +0700

    Create Property.java

---
+++ Property.java
@@ -0,0 +1,13 @@
+public class Property {
+    
+        String propertyID;
+        String description;
+        String propertyOwnerId;
+    
+        public Property(String propertyID, String description, String propertyOwnerId) {
+            this.propertyID = propertyID;
+            this.description = description;
+            this.propertyOwnerId = propertyOwnerId;
+        }
+    
+}
